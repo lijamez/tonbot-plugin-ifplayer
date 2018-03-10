42700eba0afc9366bedcd0be088898eacf60fcab
James Li <lijamez@users.noreply.github.com>
2018-03-09 23:09:34 -0800

    Fixing play request.

--- src/main/java/net/tonbot/plugin/ifplayer/IfPlayerPlayStoryRequest.java
+++ src/main/java/net/tonbot/plugin/ifplayer/IfPlayerPlayStoryRequest.java
@@ -12,7 +12,7 @@ import net.tonbot.common.Param;
 class IfPlayerPlayStoryRequest {
 
 	@Getter
-	@Param(name = "story name", ordinal = 0, description = "The name of the story to play.")
+	@Param(name = "story name", ordinal = 0, description = "The name of the story to play.", captureRemaining = true)
 	@Nonnull
 	private String storyName;
 }
